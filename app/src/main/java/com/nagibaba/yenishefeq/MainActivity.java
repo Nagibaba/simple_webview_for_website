9b15ba86c3bbb2e2f01bded9d66fbfdbe30de575
nagibaba <bnagiyev@gmail.com>
2019-04-03 14:04:09 +0400

    first commit

---
+++ app/src/main/java/com/nagibaba/yenishefeq/MainActivity.java
@@ -0,0 +1,37 @@
+package com.nagibaba.yenishefeq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.webView = (WebView) findViewById(R.id.webview);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        this.webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("http://www.yenishefeq.com");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && this.webView.canGoBack()) {
+            this.webView.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+}
