18b1563e1a777223779724b21856534ace361d04
nagibaba <bnagiyev@gmail.com>
2019-04-10 18:24:50 +0400

    added spinner progressbar between pages and edited icon

--- app/src/main/java/com/nagibaba/yenishefeq/MainActivity.java
+++ app/src/main/java/com/nagibaba/yenishefeq/MainActivity.java
@@ -1,28 +1,49 @@
 package com.nagibaba.yenishefeq;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView = null;
-
+    ProgressBar progressBar = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         this.webView = (WebView) findViewById(R.id.webview);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+//        webSettings.setDomStorageEnabled(true);
+        //webSettings.setAppCacheEnabled(true);
+//        webSettings.setLoadsImagesAutomatically(true);
 
         this.webView.setWebViewClient(new WebViewClient());
 
-        webView.loadUrl("http://www.yenishefeq.com");
+        webView.loadUrl("http://yenishefeq.com/");
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+                progressBar.setProgress(progress);
+                if (progress == 100) {
+                    progressBar.setVisibility(View.GONE);
+
+                } else {
+                    progressBar.setVisibility(View.VISIBLE);
+
+                }
+            }
+        });
     }
 
     @Override
